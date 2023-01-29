a9615cfb3739ca52b1be986364d32ff9d1c846a1
Gustavo <ingsuarezgustavo@gmail.com>
2023-01-29 16:52:57 -0400

    4

--- EntregablePokedex/src/Pages/Dashboard.jsx
+++ EntregablePokedex/src/Pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import PokemonCard from '../components/PokemonCard'
+import PokemonCard from '../Components/PokemonCard'
 
 const Dashboard = () => {
 
