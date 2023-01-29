dffa0b6ca30f12b140781e5be116be429648cef0
Gustavo <ingsuarezgustavo@gmail.com>
2023-01-29 16:07:31 -0400

    act5

---
+++ EntregablePokedex/src/App.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+import {HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./Pages/Dashboard";
+import ProtectedRoutes from "./Pages/ProtectedRoutes";
+import PokemonDetail from "./Pages/PokemonDetail";
+function App() {
+  return (
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard/:name" element={<PokemonDetail/>} />
+          <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
